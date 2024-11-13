bee4ba7cc50df316492a711b41b3988e6c610dff
NashikiAin <o.simaka18@knu.ua>
2024-11-13 10:21:09 +0200

    add mkr

---
+++ mkr/Program.cs
@@ -0,0 +1,56 @@
+﻿using mkr;
+using System;
+using System.IO;
+
+class Program
+{
+	static void Main()
+	{
+		string inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..", @"..", @"..", "input.txt");
+
+		// Check if the input file exists
+		if (!File.Exists(inputFilePath))
+		{
+			Console.WriteLine($"Not found '{inputFilePath}'.");
+			return;
+		}
+
+		var input = File.ReadAllLines(inputFilePath);
+		var parts = input[0].Split();
+
+		if (parts.Length != 2)
+		{
+			Console.WriteLine("Invalid input format. Try again. \nFor example: 3 1 (with space but only between two numbers)! ");
+			return;
+		}
+
+		// n та к повинні бути цілими числами
+		if (!int.TryParse(parts[0], out int N) || !int.TryParse(parts[1], out int K))
+		{
+			Console.WriteLine("Invalid input. N and K must be integers.");
+			return;
+		}
+
+		// Перевірка на відповідність умовам задачі
+		if (K > N || N > 9 || K < 0 || N < 1)
+		{
+			Console.WriteLine("Invalid values. Ensure that K <= N <= 9.");
+			return;
+		}
+
+
+		int[] numbers = new int[N];
+		for (int i = 0; i < N; i++)
+		{
+			numbers[i] = i + 1;
+		}
+
+
+		int count = 0;
+
+		Utils.Permute(numbers, 0, K, ref count);
+
+		Console.WriteLine("Now check the output file, there's an answer!");
+		File.WriteAllText("OUTPUT.TXT", count.ToString());
+	}
+}
---
+++ mkr/Utils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mkr
+{
+	public static class Utils
+	{
+		public static void Permute(int[] arr, int index, int K, ref int count)
+		{
+			if (index == arr.Length - 1)
+			{
+				bool isValid = true;
+				for (int i = 0; i < arr.Length - 1; i++)
+				{
+					if (Math.Abs(arr[i] - arr[i + 1]) > K)
+					{
+						isValid = false;
+						break;
+					}
+				}
+
+				if (isValid)
+					count++;
+			}
+			else
+			{
+				for (int i = index; i < arr.Length; i++)
+				{
+					Swap(ref arr[index], ref arr[i]);
+					Permute(arr, index + 1, K, ref count);
+					Swap(ref arr[index], ref arr[i]);
+				}
+			}
+		}
+
+		static void Swap(ref int a, ref int b)
+		{
+			int tmp = a;
+			a = b;
+			b = tmp;
+		}
+	}
+}
